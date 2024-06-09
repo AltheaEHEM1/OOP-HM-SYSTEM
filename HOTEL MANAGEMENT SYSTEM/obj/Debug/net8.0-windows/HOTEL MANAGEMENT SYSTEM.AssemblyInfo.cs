a603758d9ada8d963599444616501693ff08fdf8
Darben Lamonte <123372918+abcdarben@users.noreply.github.com>
2024-06-09 16:59:15 +0800

    updated gitignore

--- HOTEL MANAGEMENT SYSTEM/obj/Debug/net8.0-windows/HOTEL MANAGEMENT SYSTEM.AssemblyInfo.cs
+++ HOTEL MANAGEMENT SYSTEM/obj/Debug/net8.0-windows/HOTEL MANAGEMENT SYSTEM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ff3b103bb516c955b0b49451e817449e530d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4324571546d3ff770a6e628958c6325248e1adbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
