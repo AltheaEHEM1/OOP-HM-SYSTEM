19f3f8375eb907b7b8b754b8e755e7825b211e81
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2024-06-10 15:59:06 +0800

    UIBase

---
+++ HOTEL MANAGEMENT SYSTEM/UI/Dashboard.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace HOTEL_MANAGEMENT_SYSTEM.UI
+{
+    partial class Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(593, 227);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(276, 65);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "DASHBOARD";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox1);
+            Name = "Dashboard";
+            Size = new Size(1600, 679);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
