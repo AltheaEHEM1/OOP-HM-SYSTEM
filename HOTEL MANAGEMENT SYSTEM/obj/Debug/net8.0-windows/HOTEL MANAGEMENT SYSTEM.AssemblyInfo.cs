c15b198fd6b767af933a9c144d3f17f6f2cce819
shnlmra <141259116+shnlmra@users.noreply.github.com>
2024-06-16 19:49:48 +0800

    New base for UI

--- HOTEL MANAGEMENT SYSTEM/obj/Debug/net8.0-windows/HOTEL MANAGEMENT SYSTEM.AssemblyInfo.cs
+++ HOTEL MANAGEMENT SYSTEM/obj/Debug/net8.0-windows/HOTEL MANAGEMENT SYSTEM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698f8ee2c87f4891063421bff0fcb6d3da92ed00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f384fb1eb9e146d673d835ecab123e308363a0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOTEL MANAGEMENT SYSTEM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
